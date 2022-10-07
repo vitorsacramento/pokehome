1051b0bbc8c34b70ddb2eda9bf3b385bb22b684a
Vitor Sacramento <vitorsacramento85@gmail.com>
2022-10-07 00:08:48 -0300

    Implementação de validação para campo de busca vazio

--- src/components/Pokemons/PokemonCard/index.tsx
+++ src/components/Pokemons/PokemonCard/index.tsx
@@ -2,7 +2,6 @@ import { Pokemon } from '../../../Models/Pokemon';
 import logo from '../../../assets/img/logo.png';
 import spinner from '../../../assets/img/pokebola.png';
 import './styles.css';
-import React from 'react';
 
 type PokemonCardProps = {
     pokemon?: Pokemon | undefined;
@@ -12,53 +11,51 @@ type PokemonCardProps = {
 export const PokemonCard = ({ pokemon, loading }: PokemonCardProps) => {
 
     return (
-        <React.Fragment>
-            <div className="card">
-                <div className="card-inner">
-                    <div className="card-front">
-                        <img src={logo} alt="" width={150} />
-                    </div>
+        <div className="card">
+            <div className="card-inner">
+                <div className="card-front">
+                    <img src={logo} alt="" width={150} />
+                </div>
 
-                    <div className="card-back">
-                        <div className="card-back-inner">
-                            {loading &&
-                                <div className="spinning">
-                                    <img src={spinner} />
-                                </div>}
-                            <div className="pokemon-name">
-                                <h2>{pokemon?.name}</h2>
-                                <small>Nº {pokemon?.id < 99 ? `0${pokemon?.id}` : pokemon?.id}</small>
-                            </div>
+                <div className="card-back">
+                    <div className="card-back-inner">
+                        {loading &&
+                            <div className="spinning">
+                                <img src={spinner} />
+                            </div>}
+                        <div className="pokemon-name">
+                            <h2>{pokemon?.name}</h2>
+                            <small>Nº {pokemon?.id < 99 ? `0${pokemon?.id}` : pokemon?.id}</small>
+                        </div>
 
-                            <div className="pokemon-pic">
-                                <img src={pokemon?.sprites['front_default']} />
-                            </div>
+                        <div className="pokemon-pic">
+                            <img src={pokemon?.sprites['front_default']} />
+                        </div>
 
-                            <div className="pokemon-data">
-                                <div className="pokemon-data-type">
-                                    <span>TYPE(S):</span>
-                                    <ul>
-                                        {pokemon?.types.map((_item: any, index: any) => (
-                                            <li key={index}>
-                                                {pokemon.types[index].type['name']}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-                                <div className="pokemon-data-weight">
-                                    <span>WEIGHT:</span>
-                                    <ul>
-                                        <li>
-                                            {pokemon?.weight}
+                        <div className="pokemon-data">
+                            <div className="pokemon-data-type">
+                                <span>TYPE(S):</span>
+                                <ul>
+                                    {pokemon?.types.map((_item: any, index: any) => (
+                                        <li key={index}>
+                                            {pokemon.types[index].type['name']}
                                         </li>
-                                    </ul>
-                                </div>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            <div className="pokemon-data-weight">
+                                <span>WEIGHT:</span>
+                                <ul>
+                                    <li>
+                                        {pokemon?.weight}
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
     );
 }
--- src/components/Pokemons/index.tsx
+++ src/components/Pokemons/index.tsx
@@ -1,9 +1,11 @@
-import React, { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { api } from '../../api';
 import { Pokemon } from '../../Models/Pokemon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { PokemonCard } from './PokemonCard';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles.css';
 
 export const Pokemons = () => {
@@ -12,10 +14,15 @@ export const Pokemons = () => {
     const [loading, setLoading] = useState(false);
 
     const load = async () => {
-        setLoading(true);
-        let response = await api.loadPokemon(input);
-        setLoading(false);
-        setPokemon(response);
+        try {
+            setLoading(true);
+            let response = await api.loadPokemon(input);
+            setLoading(false);
+            setPokemon(response);
+        } catch (error) {
+            setLoading(false);
+            toast.info('Pokemon não encontrado');
+        }
     }
 
     const handleGetPokemonName = (event: ChangeEvent<HTMLInputElement>) => {
@@ -33,33 +40,36 @@ export const Pokemons = () => {
     }
 
     const handleGetPokemon = () => {
-        flipCard();
-        load();
+        if(input.match(/^[ \t]+$/)) {
+            toast.info('Digite um nome de Pokemon válido');
+        } else {
+            flipCard();
+            load();
+        }
     }
 
     return (
-        <React.Fragment>
-            <div className="pokemons">
-                <div className="pokemons-search">
-                    <input
-                        type="text"
-                        placeholder=" "
-                        id="pokemons"
-                        autoComplete='off'
-                        onChange={handleGetPokemonName}
-                        onFocus={removeFlipCard}
-                    />
-                    <label htmlFor="pokemons">Buscar Pokemons</label>
-                    <button className="btn-search" onClick={handleGetPokemon} disabled={input == '' ? true : false}>
-                        <span>BUSCAR POKEMON </span>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+        <div className="pokemons">
+            <ToastContainer />
+            <div className="pokemons-search">
+                <input
+                    type="text"
+                    placeholder=" "
+                    id="pokemons"
+                    autoComplete='off'
+                    onChange={handleGetPokemonName}
+                    onFocus={removeFlipCard}
+                />
+                <label htmlFor="pokemons">Buscar Pokemons</label>
+                <button className="btn-search" onClick={handleGetPokemon} disabled={input == '' ? true : false}>
+                    <span>BUSCAR POKEMON </span>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
 
-                <div className="pokemons-result">
-                    <PokemonCard pokemon={pokemon} loading={loading} />
-                </div>
-            </div >
-        </React.Fragment>
+            <div className="pokemons-result">
+                <PokemonCard pokemon={pokemon} loading={loading} />
+            </div>
+        </div >
     );
 }
